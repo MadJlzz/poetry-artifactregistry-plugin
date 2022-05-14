ae7e2114ec57c6b5c53973b43fc544262b04dd8d
Julien Klaer <klaer.julien@gmail.com>
2022-05-14 22:02:25 +0200

    feat: authenticating with gcloud for setting up the keyring read by poetry

--- src/poetry_artifactregistry_plugin/gcloud.py
+++ src/poetry_artifactregistry_plugin/gcloud.py
@@ -1,5 +1,29 @@
 import subprocess
+import json
+
+from cleo.io.io import IO, Verbosity
 
 def existing_gcloud() -> bool:
-    cmd = subprocess.run(['gcloud','--version'], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    cmd = subprocess.run(['gcloud', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     return cmd.returncode == 0
+
+def init_glcoud(io: IO):
+    cmd = subprocess.run(['gcloud', 'config', 'get-value', 'account'], capture_output=True)
+    if b'(unset)' in cmd.stderr:
+        io.write_line('It seems <comment>gcloud</comment> cloud is not initialized. Starting <question>`gcloud init`</question>')
+        cmd = subprocess.run(['gcloud', 'init'], check=True, stdout=subprocess.PIPE)
+        
+def load_adc(io: IO):
+    cmd = subprocess.run(['gcloud', 'config', 'config-helper', '--format=json'], capture_output=True, universal_newlines=True)
+    result = json.loads(cmd.stdout)
+    credential = result.get('credential')
+    if credential:
+        return
+    if 'access_token' in credential or 'token_expiry' in credential:
+        return
+    io.write_line('Missing or expired credentials. Refreshing ADC using <question>`gcloud auth application-default login`</question>')
+    cmd = subprocess.run(['gcloud', 'auth', 'application-default', 'login'], capture_output=True)
+
+def authenticate_gcloud(io: IO):
+    init_glcoud(io)
+    load_adc(io)
--- src/poetry_artifactregistry_plugin/plugins.py
+++ src/poetry_artifactregistry_plugin/plugins.py
@@ -3,7 +3,7 @@ from cleo.io.io import IO
 from poetry.plugins.plugin import Plugin
 from poetry.poetry import Poetry
 
-from poetry_artifactregistry_plugin.gcloud import existing_gcloud
+from poetry_artifactregistry_plugin.gcloud import existing_gcloud, authenticate_gcloud
 
 class ArtifactRegistryPlugin(Plugin):
 
@@ -12,3 +12,4 @@ class ArtifactRegistryPlugin(Plugin):
             io.write_error_line(f'<comment>poetry-artifactregistry-plugin</comment> requires <b><error>gcloud</error></b> to be functionnal.')
             io.write_error_line('Please follow instructions at [<info>https://cloud.google.com/sdk/docs/install?hl=en</info>] to use this plugin.')
             return
+        authenticate_gcloud(io)
